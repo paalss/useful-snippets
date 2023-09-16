c90490a228afb2470f9883c98384d5afd202e5c1
paalss <paalss94@gmail.com>
2023-09-16 21:13:16 +0200

    refactor(config): remove unnecessary lines

--- .eleventy.js
+++ .eleventy.js
@@ -1,10 +1,7 @@
 module.exports = function (eleventyConfig) {
-  // eleventyConfig.addPassthroughCopy({ "./public/": "/" });
   eleventyConfig.addPassthroughCopy("bundle.css");
-  // eleventyConfig.addPassthroughCopy("css/global.css");
 
   return {
-    templateFormats: ["css"],
     passthroughFileCopy: true,
   };
 };
