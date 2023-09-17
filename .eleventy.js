39adda730da156b9630bc4cf47cd2816f27bc1c0
paalss <paalss94@gmail.com>
2023-09-17 18:17:40 +0200

    feat(posts): add syntax highlighting to code snippets

--- .eleventy.js
+++ .eleventy.js
@@ -1,21 +1,6 @@
+const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
+
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("bundle.css");
-  // ! AAAA kan ikke referes til i index.html. Da blir det circular reference??
-  eleventyConfig.addCollection("AAAA", (collection) => {
-    return collection.getAll();
-  });
-  // eleventyConfig.addCollection("post", function(collectionApi) {
-  //   // console.log(111, collectionApi.isSortAscending); // dette logges i node
-  //   // console.log(111, collectionApi.isSortAscending); // dette logges i node
-  //   // const editedCollection = {
-  //   //   ...collectionApi,
-  //   //   isSortAscending: false,
-  //   //   isSortNumeric: true,
-  //   // };
-  //   // console.log(222, editedCollection)
-  //   return collectionApi.getAll();
-  // })
-  // eleventyConfig.addCollection("post", function(collectionApi) {
-  //   return collectionApi.getAll();
-  // })
+  eleventyConfig.addPlugin(syntaxHighlight);
 };
